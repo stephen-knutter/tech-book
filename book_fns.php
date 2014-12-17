e092d9e4821a799b3df1d802407f273f2a517e57
sknutter <stephen.knutter@gmail.com>
2014-12-17 14:59:12 -0700

    Initializing Repository

--- book_fns.php
+++ book_fns.php
@@ -7,6 +7,7 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="./css/suggest.css" />
 	<link rel="stylesheet" type="text/css" href="./css/bookStyle.css" />
